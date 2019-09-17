adc3923c681318f05efd59eff4987c5ead61bdb0
pigsel <bertyaev@gmail.com>
2019-09-17 22:44:03 +0300

    создан файл

---
+++ HW_3-4.sql
@@ -0,0 +1,51 @@
+/* 
+здесь будут задания и для 3 и для 4 уроков сразу, 
+т.к. по сути одно продолжение другого.
+
+кроме того, я не совсем понял что нужно сдавать и в каком виде, 
+поэтому выложу сюда выводы из терминала при работе с моей базой данных. 
+
+*/
+
+-- вот результат урока 3 (не раскрываю подробно, т.к. не сохранил вывод терминала): 
+
+mysql> USE vk
+Reading table information for completion of table and column names
+You can turn off this feature to get a quicker startup with -A
+
+Database changed
+mysql> SHOW TABLES;
++---------------------+
+| Tables_in_vk        |
++---------------------+
+| communities         |
+| communities_users   |
+| friendship          |
+| friendship_statuses |
+| media               |
+| media_types         |
+| messages            |
+| profiles            |
+| users               |
++---------------------+
+9 rows in set (0.00 sec)
+
+-- далее я проделываю те же операции, что были обсуждены на уроке
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
