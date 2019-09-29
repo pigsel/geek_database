0d6a8a1b4515efad694640f0fbe6f1d0a7878a7d
pigsel <bertyaev@gmail.com>
2019-09-29 22:31:34 +0300

    upt

--- 06/HW_6_Igor_Bertyaev.sql
+++ 06/HW_6_Igor_Bertyaev.sql
@@ -6,9 +6,8 @@
 
 -- 1/ Проанализировать запросы, которые выполнялись на занятии, 
 -- определить возможные корректировки и/или улучшения (JOIN пока не применять).
-
-проанализировал, пока в целом понятно, но есть некоторые моменты не совсем ясные, 
-как в следующе задании например. надеюсь в дальейшем разобраться.
+проанализировал, пока в целом понятно, кроме некоторых особенностей, 
+но надеюсь со временем доразобраться
 
 
 -- 2/ Пусть задан некоторый пользователь.
@@ -16,9 +15,11 @@
 -- который больше всех общался с нашим пользоваетелем.
 
 -- сделал так будто нам задано имя 
--- как номер пользователя заменить именем пока не сообразил, надеюсь пойму позднее.
+-- (friendship status_id у меня сделано 0 = дружба, 1 = нет)
 
-SELECT to_user_id, COUNT(*)
+SELECT (SELECT CONCAT(firstname, ' ', lastname) 
+      FROM users 
+      WHERE id = to_user_id) AS friend, COUNT(*) as 'messages'
   FROM messages WHERE from_user_id = (
 	SELECT id from users WHERE firstname = 'Christine' and lastname = 'Quitzon')
       AND to_user_id IN (
@@ -31,11 +32,36 @@ SELECT to_user_id, COUNT(*)
 		    and status_id = 0
       )
 GROUP by to_user_id
-LIMIT 1;
+LIMIT 1
+;
 
 
 -- 3/ Подсчитать общее количество лайков, которые получили 10 самых молодых пользователей.
+-- тут два варианта сделал - с выводом имен и количества, 
+-- и просто общую сумму (в один вывод и то и то не удалось сделать)
+-- (я посчитал лайки которые не получили а поставили пользователи похоже,
+-- чтобы посчитать тех кто получил нужно сложнее разбираться в авторстве тех чьи вещи залайканы. 
+-- но к сожалению уже не успеваю переделать)
 
+-- вариант 1
+SELECT (SELECT CONCAT(firstname, ' ', lastname) 
+       FROM users 
+       WHERE user_id = id) AS 'user',
+       COUNT(*) as 'likes' FROM likes
+         WHERE user_id IN (
+          SELECT id from users ORDER BY (SELECT birthday FROM profiles WHERE id = user_id) DESC)
+GROUP by user_id
+limit 10
+;
+
+-- вариант 2 - тут не понял зачем в конце еще раз as su, если она есть в начале, 
+-- но без нее не работает почемуто
+SELECT sum(likes) as su FROM (
+SELECT COUNT(*) as likes FROM likes
+         WHERE user_id IN (
+          SELECT id from users ORDER BY (SELECT birthday FROM profiles WHERE id = user_id) DESC)
+GROUP by user_id
+limit 10) as su;
 
 
 -- 4/ Определить кто больше поставил лайков (всего) - мужчины или женщины?
@@ -49,4 +75,4 @@ LIMIT 1;
 
 
 
--- на этом заканчиваю, спасибо
+-- конец
