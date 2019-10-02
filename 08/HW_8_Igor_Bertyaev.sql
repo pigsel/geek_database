f97d3d898876a425aef2b9fb010e5629d21b9ea9
pigsel <bertyaev@gmail.com>
2019-10-03 01:12:14 +0300

    1 and 2 ok

--- 08/HW_8_Igor_Bertyaev.sql
+++ 08/HW_8_Igor_Bertyaev.sql
@@ -18,8 +18,28 @@ ALTER TABLE friendship
   ADD CONSTRAINT friendship_status_id_fk 
     FOREIGN KEY (status_id) REFERENCES friendship_statuses(id);
 
+ALTER TABLE likes
+  ADD CONSTRAINT likes_target_type_id_fk 
+    FOREIGN KEY (target_type_id) REFERENCES target_types(id),
+  ADD CONSTRAINT likes_user_id_fk 
+    FOREIGN KEY (user_id) REFERENCES users(id);
+    
+ALTER TABLE media
+  ADD CONSTRAINT media_media_type_id_fk 
+    FOREIGN KEY (media_type_id) REFERENCES media_types(id),
+  ADD CONSTRAINT media_user_id_fk 
+    FOREIGN KEY (user_id) REFERENCES users(id);
+
+ALTER TABLE posts
+  ADD CONSTRAINT posts_communitie_id_fk 
+    FOREIGN KEY (communitie_id) REFERENCES communities(id),
+  ADD CONSTRAINT posts_user_id_fk 
+    FOREIGN KEY (user_id) REFERENCES users(id);
+    
 
 -- 2. По созданным связям создать ER диаграмму, используя Dbeaver (приложить графический файл к ДЗ).
+-- файл приложен - vk-bert.png
+
 
 -- 3. Переписать запросы, заданые к ДЗ урока 6 с использованием JOIN (четыре запроса).
 
