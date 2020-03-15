2fcd3465b412d5c635f12aa01647884f6f7f86a1
pigsel <bertyaev@gmail.com>
2020-03-15 19:31:56 +0300

    upd

--- 04_python4_search/HW_04_01_5ka.py
+++ 04_python4_search/HW_04_01_5ka.py
@@ -3,10 +3,18 @@ import json
 import time
 
 
-url = 'https://5ka.ru/api/v2/special_offers/'
+URL = 'https://5ka.ru/api/v2/special_offers/'
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36 OPR/67.0.3575.53'}
 
 CAT_URL = 'https://5ka.ru/api/v2/categories/'
+
+def cat_links():
+    url_list = []
+    categories = requests.get(CAT_URL, headers=headers)
+    cat_dict = categories.json()
+    for i in range(len(cat_dict)):
+        cat_dict[i].get('parent_group_name')
+
 
 def x5ka(url):
     result = []
@@ -18,3 +26,6 @@ def x5ka(url):
         time.sleep(1)
     return result
 
+if __name__ == '__main__':
+     #data = x5ka(URL)
+     cat_links()
